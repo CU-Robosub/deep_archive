a17e9a080b76c9320f339aa0a7b1dd5b0b5f7a8a
jeff <jeve0658@colorado.edu>
2019-02-14 22:47:07 -0700

    robosub_model

---
+++ robosub_model/README.md
@@ -0,0 +1,2 @@
+# robosub_model
+Repo for the simulink model of the sub
---
+++ robosub_model/pwdToThrust.m
@@ -0,0 +1,116 @@
+function pwdToThrust(block)
+%MSFUNTMPL_BASIC A Template for a Level-2 MATLAB S-Function
+%   The MATLAB S-function is written as a MATLAB function with the
+%   same name as the S-function. Replace 'msfuntmpl_basic' with the 
+%   name of your S-function.
+%
+%   It should be noted that the MATLAB S-function is very similar
+%   to Level-2 C-Mex S-functions. You should be able to get more
+%   information for each of the block methods by referring to the
+%   documentation for C-Mex S-functions.
+%
+%   Copyright 2003-2010 The MathWorks, Inc.
+
+%%
+%% The setup method is used to set up the basic attributes of the
+%% S-function such as ports, parameters, etc. Do not add any other
+%% calls to the main body of the function.
+%%
+setup(block);
+
+%endfunction
+
+%% Function: setup ===================================================
+%% Abstract:
+%%   Set up the basic characteristics of the S-function block such as:
+%%   - Input ports
+%%   - Output ports
+%%   - Dialog parameters
+%%   - Options
+%%
+%%   Required         : Yes
+%%   C-Mex counterpart: mdlInitializeSizes
+%%
+function setup(block)
+
+% Register number of ports
+block.NumInputPorts  = 1;
+block.NumOutputPorts = 1;
+
+% Setup port properties to be inherited or dynamic
+block.SetPreCompInpPortInfoToDynamic;
+block.SetPreCompOutPortInfoToDynamic;
+
+% Override input port properties
+block.InputPort(1).Dimensions        = 1;
+block.InputPort(1).DatatypeID  = 0;  % double
+block.InputPort(1).Complexity  = 'Real';
+block.InputPort(1).DirectFeedthrough = true;
+
+% Override output port properties
+block.OutputPort(1).Dimensions       = 1;
+block.OutputPort(1).DatatypeID  = 0; % double
+block.OutputPort(1).Complexity  = 'Real';
+
+% Register parameters
+block.NumDialogPrms     = 0;
+
+% Register sample times
+%  [0 offset]            : Continuous sample time
+%  [positive_num offset] : Discrete sample time
+%
+%  [-1, 0]               : Inherited sample time
+%  [-2, 0]               : Variable sample time
+block.SampleTimes = [0 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'CustomSimState',  < Has GetSimState and SetSimState methods
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+block.SimStateCompliance = 'DefaultSimState';
+
+%% -----------------------------------------------------------------
+%% The MATLAB S-function uses an internal registry for all
+%% block methods. You should register all relevant methods
+%% (optional and required) as illustrated below. You may choose
+%% any suitable name for the methods and implement these methods
+%% as local functions within the same file. See comments
+%% provided for each function for more information.
+%% -----------------------------------------------------------------
+
+block.RegBlockMethod('Outputs', @Outputs);     % Required
+block.RegBlockMethod('Terminate', @Terminate); % Required
+
+%end setup
+
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlOutputs
+%%
+function Outputs(block)
+if block.InputPort(1).Data < 1480
+    block.OutputPort(1).Data = 0.0260*block.InputPort(1).Data-37.6267;
+elseif block.InputPort(1).Data >= 1530
+    block.OutputPort(1).Data = 0.0309*block.InputPort(1).Data-47.9077;
+else
+    block.OutputPort(1).Data = 0;
+end
+
+%end Outputs
+
+
+%%
+%% Terminate:
+%%   Functionality    : Called at the end of simulation for cleanup
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlTerminate
+%%
+function Terminate(block)
+
+%end Terminate
+
